cde5008d867cc7e4dddba3a16adf6f20c7bf39c3
jindac <dachanman91@gmail.com>
2017-01-11 16:56:17 -0800

    request-handler returns the content of index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,33 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
-
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+//
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode = 404;
+  var data;
+  var headers = defaultCorsHeaders;
+  console.log('url =================>', req.url);
+  console.log('method =================>', req.method);
+  headers['Content-Type'] = 'text/html; charset=utf-8';
+  if (req.url === '/' && req.method === 'GET') {
+    fs.readFile(path.join(archive.paths.siteAssets, 'index.html'), function(err, page) {
+      if (err) {
+        console.log('Failed to read file', err);
+      } else {
+        statusCode = 200;
+        console.log('statusCode ========> ', statusCode);
+        data = page.toString();
+        console.log('datatostring =========>', data);
+        res.writeHead(statusCode, headers);
+        res.end(data);
+      }
+    });
+  }
 };
